d541098dfba0e1bb4c090c64d81a1822091f06da
August <august.agren@elev.ga.ntig.se>
2020-11-24 09:58:29 +0100

    start

---
+++ NovemberProjekt/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NovemberProjekt
+{           
+    // Ett matlagningsspel där man har en viss summa att spendera, och ska välja ingredienser att lägga i en gryta.
+    // Sedan finns det en jury som gillar olika ingredienser.
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello chef!");
+        }
+    }
+}
